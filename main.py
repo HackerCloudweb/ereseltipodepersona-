346e4ffbae771198bed76f1589f460836c6c0e75
hackercloud <95941095+HackerCloudweb@users.noreply.github.com>
2022-06-05 23:16:51 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#/usr/bin/env python
+
+import random
+
+randomNum = random.randint(1,2)
+
+if randomNum == 1:
+	print("\nTu eres el tipo de persona.")
+
+else:
+	print("\nTu no eres el tipo de persona.")
+
